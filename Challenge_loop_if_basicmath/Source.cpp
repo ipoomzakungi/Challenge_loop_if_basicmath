69b01b6ead46d9875f09ab6b9514961e31ccc4be
PoomBot3 <poombotkub3@hotmail.com>
2020-08-10 15:08:57 +0700

    scanf_s

---
+++ Challenge_loop_if_basicmath/Source.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a, b, c, sum = 0, N;
+	double	av, sdTemp = 0;
+	scanf("%d %d", &a, &b);
+	if (a > b)
+	{
+		for(int i=a;i>=b;i--)
+		{
+			printf("%d ", i);
+			sum += i;
+			
+		}
+		N = a - b + 1;
+		printf("\n");
+		av = sum / (float)N;
+		for (int i = a;i >= b;i--)
+		{
+
+			sdTemp += pow(i - av, 2);
+		}
+		//printf("%lf", sdTemp);
+
+		sdTemp = sqrt(sdTemp / (N - 1));
+
+		printf("average : %.2f\n", av);
+		printf("SD      : %.2f", sdTemp);
+	}
+	else if(b>a)
+	{
+		for (int i = a;i <= b;i++)
+		{
+			printf("%d ", i);
+			sum += i;
+		}
+		N = b - a + 1;
+		printf("\n");
+		av = sum / (float)N;
+		for (int i = a;i <= b;i++)
+		{
+			
+			sdTemp += pow(i - av, 2);
+		}
+		//printf("%lf", sdTemp);
+		
+		sdTemp = sqrt(sdTemp / (N-1));
+
+		printf("average : %.2f\n", av);
+		printf("SD      : %.2f", sdTemp);
+	}
+	else
+	{
+		printf("%d\n", a);
+		printf("average : %d\n", a);
+		printf("SD      : 0");
+	}
+	
+}
